da194776d60fb90cf5e5fa266d4f864ea14e4764
Aleff <quixabaaleff@gmail.com>
2024-08-15 14:15:09 -0300

    User insert and User resource fix

--- src/main/java/com/aleffmota/project_spring/resources/UserResource.java
+++ src/main/java/com/aleffmota/project_spring/resources/UserResource.java
@@ -1,24 +1,27 @@
 package com.aleffmota.project_spring.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.aleffmota.project_spring.entities.User;
-import com.aleffmota.project_spring.repositories.UserRepository;
+import com.aleffmota.project_spring.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
 	@Autowired
-	private UserRepository service;
+	private UserService service;
 	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
@@ -29,9 +32,16 @@ public class UserResource {
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Optional<User>> findById(@PathVariable Long id){
-		Optional<User> obj = service.findById(id);
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 }

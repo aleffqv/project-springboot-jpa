3246a3092b22ae9d398d26a4107874ea5279e3c1
Aleff <quixabaaleff@gmail.com>
2024-08-15 16:19:14 -0300

    Exception Handling findById

---
+++ src/main/java/com/aleffmota/project_spring/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.aleffmota.project_spring.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+}

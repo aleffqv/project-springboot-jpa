e853864f0d2ffac299e83a9a35301f507b07e348
Aleff <quixabaaleff@gmail.com>
2024-08-16 11:01:35 -0300

    Exception handling of user deletion

--- src/main/java/com/aleffmota/project_spring/services/UserService.java
+++ src/main/java/com/aleffmota/project_spring/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.aleffmota.project_spring.entities.User;
 import com.aleffmota.project_spring.repositories.UserRepository;
+import com.aleffmota.project_spring.services.exceptions.DatabaseException;
 import com.aleffmota.project_spring.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,8 +31,15 @@ public class UserService {
 		return repository.save(obj);
 	}
 	
-	public void delete(Long id) {
-		repository.deleteById(id);
+	public void delete(Long id) {	
+		try {
+			if (!repository.existsById(id)) throw new ResourceNotFoundException(id);
+			repository.deleteById(id);
+		} catch (ResourceNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
